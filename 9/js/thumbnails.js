96f6b766ac4e1a2c8dc374f029481d2a9d60df9f
Keks <keks@htmlacademy.ru>
2024-08-12 22:13:15 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/thumbnails.js
@@ -0,0 +1,30 @@
+const thumbnailContainer = document.querySelector('.pictures');
+const thumbnailTemplate = document.querySelector('#picture')
+  .content
+  .querySelector('.picture');
+
+const getThumbnail = ({ id, url, description, likes, comments }) => {
+  const thumbnail = thumbnailTemplate.cloneNode(true);
+  const thumbnailImg = thumbnail.querySelector('.picture__img');
+
+  thumbnailImg.src = url;
+  thumbnailImg.alt = description;
+  thumbnail.dataset.thumbnailId = id;
+  thumbnail.querySelector('.picture__likes').textContent = likes;
+  thumbnail.querySelector('.picture__comments').textContent = comments.length;
+
+  return thumbnail;
+};
+
+const renderThumbnails = (photos) => {
+  const thumbnailsFragment = document.createDocumentFragment();
+  photos.forEach((photo) => {
+    thumbnailsFragment.append(getThumbnail(photo));
+  }
+  );
+
+  thumbnailContainer.append(thumbnailsFragment);
+};
+
+export { renderThumbnails, thumbnailContainer };
+
